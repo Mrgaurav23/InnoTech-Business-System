558f8e2d264c8f4b1ec2722e7f4933284c5ad68e
Mrgaurav23 <gauravsinghsisodiya963@gmail.com>
2024-10-26 17:34:49 +0530

    jaddu userInfo config

--- JAVASCRIPT/jaddu.js
+++ JAVASCRIPT/jaddu.js
@@ -3,6 +3,45 @@ const startBtn = document.querySelector("#start");
 const stopBtn = document.querySelector("#stop");
 const speakBtn = document.querySelector("#speak");
 
+// jaddu Setup
+if (localStorage.getItem("jaddu-setup") !== null) {
+    //Add info here
+}
+
+//jaddu information setup
+const setup = document.querySelector(".jaddu-setup");
+setup.style.display = "none";
+if(localStorage.getItem("jaddu-setup") === null){
+    setup.style.display = "block";
+    // setup.style.display = flex;
+    document.querySelector(".sub-btn").addEventListener("click",userInfo);
+}
+
+//UserInfo function
+function userInfo(){
+    let setupInfo = {
+        name : setup.querySelectorAll("input")[0].value,
+        Bio : setup.querySelectorAll("input")[1].value,
+        Location : setup.querySelectorAll("input")[2].value,
+        LinkedIn : setup.querySelectorAll("input")[3].value,
+        Github : setup.querySelectorAll("input")[4].value,
+    }
+
+    let testArr = [];
+    setup.querySelectorAll("input").forEach( (val) => {
+        testArr.push(val);
+    });
+
+    if (testArr.includes("")) {
+        readOut("Fill complete information sir");
+    }
+    else{
+        localStorage.clear();
+        localStorage.setItem("jaddu-setup",JSON.stringify(setupInfo));
+        setup.style.display = "none";
+    }
+}
+
 //Speech Recognition Setup
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -17,6 +56,7 @@ recognition.onresult = function(event){
     let current = event.resultIndex;
     let transcript = event.results[current][0].transcript;
     transcript = transcript.toLowerCase();
+    let userData = localStorage.getItem("jaddu-setup");
     console.log(`my words : ${transcript}`);
     if (transcript.includes("hello jadu")|| transcript.includes("hey jadu") || transcript.includes("jadoo")) {
         readOut("hello Sir");
@@ -39,6 +79,16 @@ recognition.onresult = function(event){
         input.pop();
         input = input.join("").split(" ").join("+");
         window.open(`https://google.com/search?q=${input}`)
+    }
+
+    //Github Commands
+    if (transcript.includes("open github")) {
+        readOut("Opening github sir");
+        window.open("https://github.com/")
+    }
+    if (transcript.includes("open my github profile")) {
+        readOut("Opening your github profile Sir");
+        window.open(`https://github.com/${JSON.parse(userData).Github}`);
     }
 }
 
