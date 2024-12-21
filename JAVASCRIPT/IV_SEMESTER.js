27537b246b2c86fdbc0daefa33ceb04027b67efa
Mrgaurav23 <gauravsinghsisodiya963@gmail.com>
2024-12-22 01:07:50 +0530

    IV Semester

--- JAVASCRIPT/IV_SEMESTER.js
+++ JAVASCRIPT/IV_SEMESTER.js
@@ -40,51 +40,12 @@ function locomotive() {
 }
 locomotive();
 
-//Page2 changing Subject font
-function fontChange() {
-  let page2Text = document.querySelector(".page2 h1");
-
-  let carrer = ["Operating System", "DBMS", "Machine Learning", "JAVA + DSA"];
-
-  let carrerIndex = 0;
-
-  function updateText() {
-    page2Text.innerText = ` ${carrer[carrerIndex]} `;
-    carrerIndex++;
-
-    if (carrer.length == carrerIndex) {
-      carrerIndex = 0;
-    }
-    setTimeout(updateText, 1500);
-  }
-}
-
-//page2 box transiton Text
-function page2Box() {
-  let page2Box = document.querySelectorAll(".box");
-
-  page2Box.forEach((dets) => {
-    console.log(dets.childNodes[7]);
-    dets.addEventListener("mouseenter", () => {
-      dets.childNodes[7].style.opacity = 1;
-      dets.childNodes[5].style.bottom = "15%";
-    });
-    dets.addEventListener("mouseleave", () => {
-      dets.childNodes[7].style.opacity = 0;
-      dets.childNodes[5].style.bottom = "3%";
-    });
-  });
-}
-
-
-// nav 
-
-function navbar(){
+function navbar() {
   const navBar = document.querySelector(".full-div");
   const menu = document.querySelector(".nav-inner button");
   const page1Element = document.querySelector(".page1");
   let flag = 0;
-  
+
   menu.addEventListener("click", () => {
     if (flag === 0) {
       navBar.style.top = "-150%";
@@ -95,22 +56,14 @@ function navbar(){
       menu.style.border = "0.5px solid rgb(161, 161, 161)";
       menu.style.borderRadius = "20px";
       flag = 1;
-    }
-    else{
+    } else {
       navBar.style.top = "0%";
       navBar.style.opacity = 1;
       menu.textContent = "close";
       menu.style.backgroundColor = "#FFFFFF";
-      menu.style.color = "#000000"
+      menu.style.color = "#000000";
       flag = 0;
     }
-  })
+  });
 }
-
-function call(){
-  locomotive();
-  fontChange();
-  page2Box();
-  navbar();
-}
-call();
+navbar()
