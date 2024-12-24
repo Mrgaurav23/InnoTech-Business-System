0ec5e047ac432c3c8ab9a494fef49e3d32fa08ba
Mrgaurav23 <gauravsinghsisodiya963@gmail.com>
2024-12-24 19:44:28 +0530

    IV responsive

--- JAVASCRIPT/IV_SEMESTER.js
+++ JAVASCRIPT/IV_SEMESTER.js
@@ -66,31 +66,26 @@ function navbar() {
     }
   });
 }
-navbar()
+navbar();
 
-
-const slides = document.querySelectorAll(".slide")
+const slides = document.querySelectorAll(".slide");
 var counter = 0;
-slides.forEach(
-  (slide,index) => {
-    slide.style.left = `${index * 100}%`
-  }
-) 
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
 
 const goPrev = () => {
-  counter--
-  slideImage()
-}
+  counter--;
+  slideImage();
+};
 
 const goNext = () => {
-  counter++
-  slideImage()
-}
+  counter++;
+  slideImage();
+};
 
 const slideImage = () => {
-  slides.forEach(
-    (slide) => {
-      slide.style.transform = `translateX(-${counter * 125}%)`
-    }
-  )
-}
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 112}%)`;
+  });
+};
